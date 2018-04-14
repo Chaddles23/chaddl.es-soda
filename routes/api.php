34d9af38fc6e7b943487766daf7f61ca05c5b7e8
Caleb Chadwick <Caleb@Calebs-MacBook-Pro.local>
2018-04-14 14:31:38 +0930

    any git push

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
     //Route::any('holiday-mode', 'HolidayController@isHolidayMode');
-    Route::post('enabled', 'HassIOController@isEnabled');
+    Route::any('enabled', 'HassIOController@isEnabled');
 });
