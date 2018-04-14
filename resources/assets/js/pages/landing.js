691856a06390f7bb9acdb33bb0237939a34c344c
Caleb Chadwick <Caleb@Calebs-MacBook-Pro.local>
2018-04-14 21:31:29 +0930

    overflow

--- resources/assets/js/pages/landing.js
+++ resources/assets/js/pages/landing.js
@@ -1,12 +1,8 @@
 if( $('#landing').length ){
-	$(document).on('ready',function(){
-
-		Snowflakes({
-			zIndex: 1,
-			minSize: 50,
-	        maxSize: 100,	
-	        minOpacity: 0.75,
-		});
-
+	Snowflakes({
+		zIndex: 1,
+		minSize: 50,
+        maxSize: 100,	
+        minOpacity: 0.75,
 	});
 }
